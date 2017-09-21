9f154fbd3922fd4280fbcf6b1e093719731093d3
hattori <trinity.bb.19@gmail.com>
2017-09-21 11:50:22 +0000

    make toppage

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  get 'toppages/index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: "tasks#index"
+  root to: "toppages#index"
   
   resources :tasks #テーブル名（設定したmodel名の小文字複数形）
 end
